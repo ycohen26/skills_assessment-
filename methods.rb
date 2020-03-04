3328555fadb1c5502ab192c316e6e0e63d18d9b7
Yossi <yosef.d.cohen@gmail.com>
2020-03-03 18:08:42 -0600

    add additional practice problems

--- methods.rb
+++ methods.rb
@@ -1,66 +1,86 @@
 # # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
 
-# print "Please tye a word: "
-# response = gets.chomp.upcase
+print "Please tye a word: "
+response = gets.chomp.upcase
 
-# puts response
+puts response
 
 # # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
 
-# print "Please enter a number: "
-# response = gets.chomp.to_i
+print "Please enter a number: "
+response = gets.chomp.to_i
 
-# if response > 100
-#   puts "That's a big number!"
-# end
+if response > 100
+  puts "That's a big number!"
+end
 
 # # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
 
-# responses = []
+responses = []
 
-# print "Enter two numbers: "
-# 2.times do 
-#   response = gets.chomp.to_i
-#   responses << response
-# end 
+print "Enter two numbers: "
+2.times do 
+  response = gets.chomp.to_i
+  responses << response
+end 
 
-# p responses.sum
+p responses.sum
 
 # # 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
 
-# print "Enter a word: "
-# response = gets.chomp
+print "Enter a word: "
+response = gets.chomp
 
-# p response.reverse
+p response.reverse
 
 # # 5. Write a program that asks the user to enter a number, then prints the number times 10.
 
-# print "Enter a number and I'll tell it to you times 10: "
-# response = gets.chomp.to_i
+print "Enter a number and I'll tell it to you times 10: "
+response = gets.chomp.to_i
 
-# p response * 10
+p response * 10
 
 # 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
 
-# responses = []
+responses = []
 
-# print "Enter two words: "
-# 2.times do
-#   response = gets.chomp
-#   responses << response
-# end
+print "Enter two words: "
+2.times do
+  response = gets.chomp
+  responses << response
+end
 
-# puts responses.join(" ").upcase
+puts responses.join(" ").upcase
   
 # 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
 
-# puts "Write a word: "
-# response = gets.chomp
+puts "Write a word: "
+response = gets.chomp
 
-# p response.length  
+p response.length  
 
 # 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
 
+puts "Enter a number, positive or negative: "
+response = gets.chomp.to_i
+
+if response < 0
+  puts "That's a negative number."
+end
+
 # 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
 
+puts "Enter two numbers."
+response1 = gets.chomp.to_i
+response2 = gets.chomp.to_i
+
+p response1 * response2
+
 # 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+
+puts "Please enter a word: "
+response = gets.chomp
+
+if response.length > 5
+  puts "That's a long word!"
+end
